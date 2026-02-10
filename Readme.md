55e72cfdf109c55b4f42d390a4c7e061decb07ed
Daniel Otulagun <daniel.otulagun@zeecrest.com>
2026-02-10 21:56:15 +0100

    Build a simple agent with local running ollama

---
+++ Readme.md
@@ -0,0 +1,3 @@
+A course from Coursera
+
+Building Autonomous AI Agents with LangGraph
---
+++ simple_agent.py
@@ -0,0 +1,16 @@
+from ollama import chat
+from ollama import ChatResponse
+
+response: ChatResponse = chat(model='gemma:2b', messages=[
+  {
+    'role': 'system',
+    'content': 'You are a helpful assistant.',
+  },
+  {
+    'role': 'user',
+    'content': 'Can the ocean water level reduce due to gloabl warming?',
+  },
+])
+print(response['message']['content'])
+# or access fields directly from the response object
+print(response.message.content)
